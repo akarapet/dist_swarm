b678c2c4f539c1293cfe7cb6a2029e6f2e8bfc1d
Aren <karapetyanaren@gmail.com>
2020-01-24 17:16:45 +0000

    first commit

---
+++ centralised_collision_avoidance.py
@@ -0,0 +1,9 @@
+import cflib.crtp
+from cflib.crazyflie import Crazyflie
+from cflib.crazyflie.syncCrazyflie import SyncCrazyflie
+
+URI = 'radio://0/90/2M'
+
+if __name__ == '__main__':
+
+    print('Hello')
